b712dfde758ef5488fe26600ab13b23210adcb2c
Motya22 <motya2222@gmail.com>
2022-03-15 12:48:59 +0300

    feat: done this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = str.split('');
+  const stack = [];
+	
+  for (const bracket of brackets) {
+    const lastOfStack = stack[stack.length - 1];
+    const neededPair = bracketsConfig.find((bracketsPair) => bracketsPair[0] === lastOfStack);
+        
+    if (stack.length && neededPair && bracket === neededPair[1]) {
+      stack.pop();
+    } else  {
+      stack.push(bracket);
+    }
+  }
+  
+  return !Boolean(stack.length);
 }
